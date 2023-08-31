8675d5db35967022e64b7c0fcf0376c5ae90254b
ihsavru <vurvashi11@gmail.com>
2023-08-31 12:42:24 +0530

    Add prettier & eslint

--- solutions/promises_async_await/1.js
+++ solutions/promises_async_await/1.js
@@ -1,19 +1,18 @@
-function getPromise() {
-    return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            resolve("Resolved");
-        },2000);
-    })
+function getPromise () {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Resolved')
+    }, 2000)
+  })
 }
 
 const firstFunction = () => {
-    getPromise()
-        .then((res) => console.log(res));
+  getPromise().then((res) => console.log(res))
 }
 
 const secondFunction = async () => {
-    const res = await getPromise();
-    console.log(res);
+  const res = await getPromise()
+  console.log(res)
 }
-firstFunction();
-secondFunction();
+firstFunction()
+secondFunction()
--- solutions/promises_async_await/2.js
+++ solutions/promises_async_await/2.js
@@ -1,15 +1,15 @@
-const URL = "https://api.chucknorris.io/jokes/random"
+const URL = 'https://api.chucknorris.io/jokes/random'
 
 const fetchUsingThen = () => {
-    fetch(URL)
-        .then(res => res.json())
-        .then(res => console.log(res.value))
+  fetch(URL)
+    .then((res) => res.json())
+    .then((res) => console.log(res.value))
 }
 
 const fetchUsingAsynAwait = async () => {
-    const res = await fetch(URL)
-    const data = await res.json();
-    console.log(data.value);
+  const res = await fetch(URL)
+  const data = await res.json()
+  console.log(data.value)
 }
 
 fetchUsingThen()
