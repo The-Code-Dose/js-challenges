b5efba5a219a77725ceead504bba975389f12dd9
Pushpak Raut <pushpakraut123@gmail.com>
2023-08-31 22:20:36 +0530

    solution: solved the 4th challenge of promises async await (4)

---
+++ solutions/promises_async_await/4.js
@@ -0,0 +1,20 @@
+const firstPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("1. Finished");
+  }, 1000);
+});
+const secondPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("2. Finished");
+  }, 2000);
+});
+const thirdPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("3. Finished");
+  }, 3000);
+});
+
+Promise.all([firstPromise, secondPromise, thirdPromise])
+    .then((res) =>
+        console.log(res),
+    );
