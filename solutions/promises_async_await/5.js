5aaef6fe74b573482e4d8b48e39492aef9a975a5
Pushpak Raut <pushpakraut123@gmail.com>
2023-08-31 22:57:15 +0530

    solution: solved the 5th challenge of promises async await (5)

---
+++ solutions/promises_async_await/5.js
@@ -0,0 +1,46 @@
+const URL = "https://whatthecommit.com/index.json";
+
+
+//using .then and .catch
+const fetchUsingThen = () => {
+    let requests = [];
+    for(let i = 0;i < 10; i++) {
+        requests.push(fetch(URL))
+    }
+
+    Promise.all(requests)
+    .then(responses => Promise.all(responses.map(result => result.json())))
+    .then(data => console.log(data))
+    .catch(err => console.log("Error: " + err))
+}
+
+fetchUsingThen();
+
+//fetch using async await
+const fetchAsyncAwait = async () =>{
+    let requests = [];
+    for(let i = 0;i < 10; i++) {
+        requests.push(fetch(URL))
+    }
+
+    try{
+        const responses = await Promise.all(requests);
+        const data = await Promise.all(responses.map(result => result.json()))
+        console.log(data)
+    }
+    catch(error){
+        console.log("Error: " + error);
+    }
+}
+
+fetchAsyncAwait()
+
+
+// simple fetch for 1 time
+const getFetch = () => {
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+getFetch()
