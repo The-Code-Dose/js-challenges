3c6321e8ccecd621d6ce1c67ba8bdfdb6505fe19
Pushpak Raut <pushpakraut123@gmail.com>
2023-09-01 01:36:34 +0530

    solution: updated the 2nd challenge of array methods (2)

--- solutions/array_methods/2.js
+++ solutions/array_methods/2.js
@@ -1,40 +1,41 @@
 const employees = [
-    {
-      name: "Michael Scott",
-      department: "Management",
-    },
-    {
-      name: "Dwight Schrute",
-      department: "Sales",
-    },
-    {
-      name: "Jim Halpert",
-      department: "Sales",
-    },
-    {
-      name: "Pam Beesly",
-      department: "Admin",
-    },
-    {
-      name: "Angela Martin",
-      department: "Accounting",
-    },
-    {
-      name: "Kevin Malone",
-      department: "Accounting",
-    },
-    {
-      name: "Andy Bernard",
-      department: "Sales",
-    },
-    {
-      name: "Oscar Martinez",
-      department: "Accounting",
-    },
-  ];
+  {
+    name: "Michael Scott",
+    department: "Management",
+  },
+  {
+    name: "Dwight Schrute",
+    department: "Sales",
+  },
+  {
+    name: "Jim Halpert",
+    department: "Sales",
+  },
+  {
+    name: "Pam Beesly",
+    department: "Admin",
+  },
+  {
+    name: "Angela Martin",
+    department: "Accounting",
+  },
+  {
+    name: "Kevin Malone",
+    department: "Accounting",
+  },
+  {
+    name: "Andy Bernard",
+    department: "Sales",
+  },
+  {
+    name: "Oscar Martinez",
+    department: "Accounting",
+  },
+];
 
-
-const sales = employees.filter(employee => employee.department === "Sales");
-console.log(sales)
-const accounting = employees.filter(employee => employee.department === "Accounting");
-console.log(accounting)
+const sales = employees.filter((employee) => employee.department === "Sales");
+console.log(sales);
+const accounting = employees.filter(
+  (employee) => employee.department === "Accounting",
+);
+console.log(accounting);
