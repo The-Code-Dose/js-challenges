5f550971fee2c181b3bc7e1c738b1dbd5693b286
ihsavru <vurvashi11@gmail.com>
2023-10-27 13:59:55 +0530

    Format solutions

--- solutions/array_methods/1.js
+++ solutions/array_methods/1.js
@@ -1,13 +1,7 @@
 const numbers = [15, 25, 35, 45, 55, 65];
-
-let sumUsingForEach = 0;
-
+let sum = 0;
 numbers.forEach((num) => {
-  sumUsingForEach += num;
+  sum += num;
 });
 
-console.log(sumUsingForEach);
-
-const sumUsingReduce = numbers.reduce((acc, curr) => acc + curr);
-
-console.log(sumUsingReduce);
+console.log(sum);
